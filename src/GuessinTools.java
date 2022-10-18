a524f01ceb1b750c3ede494c95830a37b817402a
Radek Cihlář <radekcihlar@radimsu.eu>
2022-10-18 09:57:28 +0200

    zas update

---
+++ src/GuessinTools.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GuessinTools {
+    private ArrayList<Integer> listOfOptions = new ArrayList<>(Arrays.asList(1,2,3,4,1,2,3,4,5,6,7,8,5,6,7,8));
+
+}
