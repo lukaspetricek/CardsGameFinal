9adbbcd4ba710762d11af6f82e8bc0b37a9ef9a5
Radek Cihlář <radekcihlar@radimsu.eu>
2022-10-18 09:53:05 +0200

    zadání update

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,20 @@ public class Main {
 //The user enters two pairs of coordinates, to turn two “cards”. If the “cards” are equal they will stay faced up.
 //If not, they should be turned again to be hidden.
 
+
+        // Připravit ArrayList, ve kterém  budou možnosti pro seznam
+        // Pak přiravit Array o specifické délce, do kterého napustíme věci z Arraylistu
+        // Pak vytvoříme Array, kterje se zobrazí lidem při hře.
+        //  Když se uhádne pozice v schovanem Arrayi, tak se přepíšoe záznam do Arraye pro hráče
+        // když ne opakujeme dokud se vše neuhádne, uhádnutí končí, když zbývá poslední dvojice na nalezení.
+        // je možnost počítat, kolik kol to trvalo
+
         System.out.println("#   #   #   #");
         System.out.println("#   #   #   #");
         System.out.println("#   #   #   #");
         System.out.println("#   #   #   #");
+
+
 
     }
 }
