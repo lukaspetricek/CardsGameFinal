42dc2fd6e6137a82eff10522a609069138566895
Lukas Petricek <lukas.petricek.cz@gmail.com>
2022-10-18 10:46:14 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 ///If you are doing a console-based version, use numbers instead of the usual cards or objects, and a board like:
@@ -7,6 +9,39 @@ public class Main {
 //# # # #
 //The user enters two pairs of coordinates, to turn two “cards”. If the “cards” are equal they will stay faced up.
 //If not, they should be turned again to be hidden.
+
+        ArrayList<Integer> allCards = new ArrayList<>();
+
+        allCards.add(1);
+        allCards.add(1);
+        allCards.add(2);
+        allCards.add(2);
+        allCards.add(3);
+        allCards.add(3);
+        allCards.add(4);
+        allCards.add(4);
+        allCards.add(5);
+        allCards.add(5);
+        allCards.add(6);
+        allCards.add(6);
+        allCards.add(7);
+        allCards.add(7);
+        allCards.add(8);
+        allCards.add(8);
+
+        int min = 0;
+        int random_card;
+        Integer[][] hiddenCards = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                random_card = (int) Math.floor(Math.random() * (allCards.size() - min + 1) + min);
+                hiddenCards[i][j] = random_card;
+                System.out.println(hiddenCards[i][j]);
+            }
+        }
+
+        System.out.println("Cards game");
 
 
         // Připravit ArrayList, ve kterém  budou možnosti pro seznam
@@ -26,7 +61,10 @@ public class Main {
 //        System.out.println("#   #   #   #");
 //        System.out.println("#   #   #   #");
 
-char[][] showUser =     {{'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}};
+
+
+
+char[][] showUser =     {{'1', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}};
 
         for (int i = 0; i < 4; i++){
             for(int j = 0; j < 4; j++){
