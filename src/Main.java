4b1fe20bde830ac4f7e8c491469f7db4104e4ddb
Radek Cihlář <radekcihlar@radimsu.eu>
2022-10-18 09:38:54 +0200

    -

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("#   #   #   #");
+///If you are doing a console-based version, use numbers instead of the usual cards or objects, and a board like:
+//# # # #
+//# # # #
+//# # # #
+//# # # #
+//The user enters two pairs of coordinates, to turn two “cards”. If the “cards” are equal they will stay faced up.
+//If not, they should be turned again to be hidden.
+
         System.out.println("#   #   #   #");
         System.out.println("#   #   #   #");
         System.out.println("#   #   #   #");
