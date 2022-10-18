0ae4365964537f333c17dcae1f331e242a9ca89a
Radek Cihlář <radekcihlar@radimsu.eu>
2022-10-18 10:44:27 +0200

    showfield and guess options;

--- src/GuessinTools.java
+++ src/GuessinTools.java
@@ -2,8 +2,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GuessinTools {
+    public GuessinTools() {
+    }
     private ArrayList<Integer> listOfOptions = new ArrayList<>(Arrays.asList(1,2,3,4,1,2,3,4,5,6,7,8,5,6,7,8));
+    private char[][] showUser =  {{'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}};
 
 
+
+    public void showField(){
+    for (int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            System.out.print(showUser[i][j] + " ");
+        }
+        System.out.println();
+    }
+}
+    public void guess(int pos1, int pos2){
+        showUser[pos1-1][pos2-1] = '9';
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        GuessinTools play = new GuessinTools();
 ///If you are doing a console-based version, use numbers instead of the usual cards or objects, and a board like:
 //# # # #
 //# # # #
@@ -26,13 +27,11 @@ public class Main {
 //        System.out.println("#   #   #   #");
 //        System.out.println("#   #   #   #");
 
-char[][] showUser =     {{'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}};
 
-        for (int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                System.out.print(showUser[i][j] + " ");
-            }
-            System.out.println();
-        }
+        //Show board to user
+        play.showField();
+        play.guess(1,1);
+        System.out.println();
+        play.showField();
     }
 }
