7bdf05a795155bcf5407d70c7bf49184c4dccc16
Lukas Petricek <lukas.petricek.cz@gmail.com>
2022-10-19 21:42:11 +0200

    handled input, need more testing, clearing duplicated ifs in GT class

--- src/GuessinTools.java
+++ src/GuessinTools.java
@@ -9,16 +9,17 @@ public class GuessinTools {
     public GuessinTools() {
     }
 
-    public static int counter = 0;
-    private ArrayList<Character> listOfOptions = new ArrayList<>(Arrays.asList('1', '2', '3', '4', '1', '2', '3', '4', '5', '6', '7', '8', '5', '6', '7', '8'));
-    private ArrayList<Character> listOfOptionsSource = new ArrayList<>(Arrays.asList('1', '2', '3', '4', '1', '2', '3', '4', '5', '6', '7', '8', '5', '6', '7', '8'));
+    private static int counter = 0;
+    //private ArrayList<Character> listOfOptions = new ArrayList<>(Arrays.asList('1', '2', '3', '4', '1', '2', '3', '4', '5', '6', '7', '8', '5', '6', '7', '8'));
 
     private char[][] showUser = {{'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}};
     private char[][] guessingField = new char[4][4];
 
+
     // Generuje hrací pole, které neukazujeme hráči
     public void generateHiddenField() {
-        Collections.copy(listOfOptions, listOfOptionsSource);
+        ArrayList<Character> listOfOptions = new ArrayList<>(Arrays.asList('1', '2', '3', '4', '1', '2', '3', '4', '5', '6', '7', '8', '5', '6', '7', '8'));
+        char[][] showUser = {{'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}};
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 int random = (int) Math.floor(Math.random() * listOfOptions.size());
@@ -48,56 +49,64 @@ public class GuessinTools {
         }
     }
 
-        public void guess ( int pos1, int pos2, int pos3, int pos4) throws InterruptedException {
-//            showHiddenField();
-//            if (guessingField[pos1][pos2] != '#' && guessingField[pos3][pos4] != '#') {
-//                if ((pos1 < 4) && (pos2 < 4) && (pos3 < 4) && (pos4 < 4)) {
-            if ((pos1 < 4) && (pos2 < 4) && (pos3 < 4) && (pos4 < 4)) {
-                if (guessingField[pos1][pos2] != '#' && guessingField[pos3][pos4] != '#') {
-                    if ((pos1 == pos3 && pos2 == pos4)) {
-                        System.out.println("Enter different card coordinates!");
-                        System.out.println();
-                        showPlayingField();
-                    } else if (guessingField[pos1][pos2] == guessingField[pos3][pos4]) {
-                        showUser[pos1][pos2] = guessingField[pos1][pos2];
-                        showUser[pos3][pos4] = guessingField[pos3][pos4];
-                        guessingField[pos3][pos4] = '#';
-                        guessingField[pos1][pos2] = '#';
+    public void guess(int pos1, int pos2, int pos3, int pos4) throws InterruptedException {
+        if ((pos1 < 4) && (pos2 < 4) && (pos3 < 4) && (pos4 < 4)) {
+            if (guessingField[pos1][pos2] != '#' && guessingField[pos3][pos4] != '#') {
+                if ((pos1 == pos3 && pos2 == pos4)) {
+                    System.out.println("Enter different card coordinates!");
+                    System.out.println();
+                    showPlayingField();
+                } else if (guessingField[pos1][pos2] == guessingField[pos3][pos4]) {
+                    showUser[pos1][pos2] = guessingField[pos1][pos2];
+                    showUser[pos3][pos4] = guessingField[pos3][pos4];
+                    guessingField[pos3][pos4] = '#';
+                    guessingField[pos1][pos2] = '#';
 
-                        System.out.println("Correct!");
-                        counter++;
+                    System.out.println("Correct!");
+                    counter++;
 
-                        showPlayingField();
-                    } else {
-                        System.out.println("Inccorect!");
-                        System.out.println();
-                        showUser[pos1][pos2] = guessingField[pos1][pos2];
-                        showUser[pos3][pos4] = guessingField[pos3][pos4];
-                        showPlayingField();
-                        TimeUnit.SECONDS.sleep(5);
-                        System.out.println();
-                        showUser[pos1][pos2] = '#';
-                        showUser[pos3][pos4] = '#';
-                        showPlayingField();
-                    }
+                    showPlayingField();
                 } else {
-                    System.out.println("You have already guessed this combination!");
+                    System.out.println("Inccorect!");
+                    System.out.println();
+                    showUser[pos1][pos2] = guessingField[pos1][pos2];
+                    showUser[pos3][pos4] = guessingField[pos3][pos4];
+                    showPlayingField();
+                    TimeUnit.SECONDS.sleep(5);
+                    System.out.println();
+                    showUser[pos1][pos2] = '#';
+                    showUser[pos3][pos4] = '#';
+                    showPlayingField();
                 }
             } else {
-                System.out.println("Enter valid numbers!");
+                System.out.println("You have already guessed this combination!");
             }
+        } else {
+            System.out.println("Enter valid numbers!");
         }
+    }
 
-// Logika zda user Vyhrál
-    public boolean alreadyWon(){
+    // Logika zda user Vyhrál
+    public boolean alreadyWon() {
         return counter == 8;
-        }
+    }
 
     public static int getCounter() {
         return counter;
-        }
+    }
 
     public static void setCounter(int counter) {
         GuessinTools.counter = counter;
+    }
+
+// reset hidden field and reset field of # showed to user
+    public void resetGame() {
+        generateHiddenField();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                showUser[i][j] = '#';
+                System.out.println(showUser[i][j]);
+            }
         }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,71 +4,233 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         GuessinTools play = new GuessinTools();
         Scanner scanner = new Scanner(System.in);
-        int pos1, pos2, pos3, pos4;
         boolean flag = true;
         boolean playAgain = true;
-
         play.generateHiddenField();
+        play.showPlayingField();
+        play.showHiddenField();
 
-    while(playAgain){
+        while (playAgain) {
+            while (!play.alreadyWon()) {
+// show board to user only the first time
+//                if (flag) {
+//                    play.showPlayingField();
+//                    flag = false;
+//                }
+// cheat sheet to play faster / 2do delete
+                //play.showHiddenField();
 
-        //Show board to user only the first time
-        while(!play.alreadyWon()){
-            if(flag){
-                play.showPlayingField();
-                flag = false;
+// get user input validation by brute force :-D
+            System.out.println("Enter row 1-4 of card A.");
+                    Scanner userChoice = new Scanner(System.in);
+                    int userInput = 0, n = 1;
+                    boolean valid;
+                    do {
+                        do {
+                            valid = true;
+                            try {
+                                userInput = Integer.parseInt(userChoice.nextLine());
+                            } catch (NumberFormatException e) {
+                                System.out.println("Enter only valid choice (1-4)!");
+                                valid = false;
+                            }
+                        } while (!valid);
+                        switch (userInput) {
+                            case 1:
+                                System.out.println("1");
+                                n = 2;
+                                break;
+
+                            case 2:
+                                System.out.println("2");
+                                n = 2;
+                                break;
+
+                            case 3:
+                                System.out.println("3");
+                                n = 2;
+                                break;
+
+                            case 4:
+                                System.out.println("4");
+                                n = 2;
+                                break;
+                            default:
+                                System.out.println("Invalid input. Valid values are 1-4!");
+                        }
+                    } while (n == 1);
+                    int pos1 = userInput - 1;
+
+                System.out.println("Enter column 1-4 of card A.");
+                userInput = 0;
+                n = 1;
+                do {
+                    do {
+                        valid = true;
+                        try {
+                            userInput = Integer.parseInt(userChoice.nextLine());
+                        } catch (NumberFormatException e) {
+                            System.out.println("Enter only valid choice (1-4)!");
+                            valid = false;
+                        }
+                    } while (!valid);
+                    switch (userInput) {
+                        case 1:
+                            System.out.println("1");
+                            n = 2;
+                            break;
+
+                        case 2:
+                            System.out.println("2");
+                            n = 2;
+                            break;
+
+                        case 3:
+                            System.out.println("3");
+                            n = 2;
+                            break;
+
+                        case 4:
+                            System.out.println("4");
+                            n = 2;
+                            break;
+                        default:
+                            System.out.println("Invalid input. Valid values are 1-4!");
+                    }
+                } while (n == 1);
+                int pos2 = userInput - 1;
+
+                System.out.println("Enter row 1-4 of card B.");
+                userInput = 0;
+                n = 1;
+                do {
+                    do {
+                        valid = true;
+                        try {
+                            userInput = Integer.parseInt(userChoice.nextLine());
+                        } catch (NumberFormatException e) {
+                            System.out.println("Enter only valid choice (1-4)!");
+                            valid = false;
+                        }
+                    } while (!valid);
+                    switch (userInput) {
+                        case 1:
+                            System.out.println("1");
+                            n = 2;
+                            break;
+
+                        case 2:
+                            System.out.println("2");
+                            n = 2;
+                            break;
+
+                        case 3:
+                            System.out.println("3");
+                            n = 2;
+                            break;
+
+                        case 4:
+                            System.out.println("4");
+                            n = 2;
+                            break;
+                        default:
+                            System.out.println("Invalid input. Valid values are 1-4!");
+                    }
+                } while (n == 1);
+                int pos3 = userInput - 1;
+
+                System.out.println("Enter column 1-4 of card B.");
+                userInput = 0;
+                n = 1;
+                do {
+                    do {
+                        valid = true;
+                        try {
+                            userInput = Integer.parseInt(userChoice.nextLine());
+                        } catch (NumberFormatException e) {
+                            System.out.println("Enter only valid choice (1-4)!");
+                            valid = false;
+                        }
+                    } while (!valid);
+                    switch (userInput) {
+                        case 1:
+                            System.out.println("1");
+                            n = 2;
+                            break;
+
+                        case 2:
+                            System.out.println("2");
+                            n = 2;
+                            break;
+
+                        case 3:
+                            System.out.println("3");
+                            n = 2;
+                            break;
+
+                        case 4:
+                            System.out.println("4");
+                            n = 2;
+                            break;
+                        default:
+                            System.out.println("Invalid input. Valid values are 1-4!");
+                    }
+                } while (n == 1);
+                int pos4 = userInput -1;
+
+                play.guess(pos1, pos2, pos3, pos4);
             }
-            //get user input
-            System.out.println("Enter positive number coordinates of card A (1-" + 4 + "), (1-" + 4 + "): ");
 
-            do {
-                System.out.print("Please enter a Row: ");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("That's not a number!");
-                    System.out.println();
-                    System.out.print("Enter row again: ");
-                    scanner.next(); // this is important!
-                }
-                pos1 = scanner.nextInt() -1;
-            } while (pos1 < -1);
-
-            do {
-                System.out.print("Please enter a column: ");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("That's not a number!");
-                    System.out.println();
-                    System.out.print("Enter column again: ");
-                    scanner.next(); // this is important!
-                }
-                pos2 = scanner.nextInt() -1;
-            } while (pos2 < -1);
-
-            System.out.println("Enter positive number coordinates of card B (1-" + 4 + "), (1-" + 4 + "): ");
-
-            do {
-                System.out.print("Please enter a row: ");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("That's not a number!");
-                    System.out.println();
-                    System.out.print("Enter row again: ");
-                    scanner.nextLine(); // this is important!
-                }
-                pos3 = scanner.nextInt() - 1;
-            } while (pos3 < - 1);
-
-            do {
-                System.out.print("Please enter a column: ");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("That's not a number!");
-                    System.out.println();
-                    System.out.print("Enter column again: ");
-                    scanner.nextLine(); // this is important!
-                }
-                pos4 = scanner.nextInt() - 1;
-            } while (pos4 < - 1);
-
-            play.guess(pos1, pos2, pos3, pos4);
-        }
+//            System.out.println("Enter positive number coordinates of card A (1-" + 4 + "), (1-" + 4 + "): ");
+//
+//            do {
+//                System.out.print("Please enter a Row: ");
+//                while (!scanner.hasNextInt()) {
+//                    System.out.println("That's not a number!");
+//                    System.out.println();
+//                    System.out.print("Enter row again: ");
+//                    scanner.next(); // this is important!
+//                }
+//                pos1 = scanner.nextInt() -1;
+//            } while (pos1 < -1);
+//
+//            do {
+//                System.out.print("Please enter a column: ");
+//                while (!scanner.hasNextInt()) {
+//                    System.out.println("That's not a number!");
+//                    System.out.println();
+//                    System.out.print("Enter column again: ");
+//                    scanner.next(); // this is important!
+//                }
+//                pos2 = scanner.nextInt() -1;
+//            } while (pos2 < -1);
+//
+//            System.out.println("Enter positive number coordinates of card B (1-" + 4 + "), (1-" + 4 + "): ");
+//
+//            do {
+//                System.out.print("Please enter a row: ");
+//                while (!scanner.hasNextInt()) {
+//                    System.out.println("That's not a number!");
+//                    System.out.println();
+//                    System.out.print("Enter row again: ");
+//                    scanner.nextLine(); // this is important!
+//                }
+//                pos3 = scanner.nextInt() - 1;
+//            } while (pos3 < - 1);
+//
+//            do {
+//                System.out.print("Please enter a column: ");
+//                while (!scanner.hasNextInt()) {
+//                    System.out.println("That's not a number!");
+//                    System.out.println();
+//                    System.out.print("Enter column again: ");
+//                    scanner.nextLine(); // this is important!
+//                }
+//                pos4 = scanner.nextInt() - 1;
+//            } while (pos4 < - 1);
+//
+//            play.guess(pos1, pos2, pos3, pos4);
+//        }
 
         //If user wins show whole field and ask for playing Again.
         System.out.println("You have won!");
@@ -77,15 +239,15 @@ public class Main {
         System.out.print("Do you wanna play again?: [Y/N]");
         String userDecide = scanner.next();
 
-        if(userDecide.equalsIgnoreCase("N") || userDecide.equalsIgnoreCase("No")){
+        if (userDecide.equalsIgnoreCase("N") || userDecide.equalsIgnoreCase("No")) {
             System.out.println("K thx bye!");
             playAgain = false;
         } else {
             play.setCounter(0);
-            play.generateHiddenField();
+            play.resetGame();
             flag = true;
-        }
 
+        }
     }
         System.out.println("Thanks for playing!");
     }
