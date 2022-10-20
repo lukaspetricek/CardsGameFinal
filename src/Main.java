24cbab9f5d700c46dfcef438fa9d04333ff7d282
Lukas Petricek <lukas.petricek.cz@gmail.com>
2022-10-20 08:49:35 +0200

    handled input, need more testing, clearing duplicated ifs in GT class

--- src/Main.java
+++ src/Main.java
@@ -153,57 +153,6 @@ public class Main {
                 play.guess(pos1, pos2, pos3, pos4);
             }
 
-//            System.out.println("Enter positive number coordinates of card A (1-" + 4 + "), (1-" + 4 + "): ");
-//
-//            do {
-//                System.out.print("Please enter a Row: ");
-//                while (!scanner.hasNextInt()) {
-//                    System.out.println("That's not a number!");
-//                    System.out.println();
-//                    System.out.print("Enter row again: ");
-//                    scanner.next(); // this is important!
-//                }
-//                pos1 = scanner.nextInt() -1;
-//            } while (pos1 < -1);
-//
-//            do {
-//                System.out.print("Please enter a column: ");
-//                while (!scanner.hasNextInt()) {
-//                    System.out.println("That's not a number!");
-//                    System.out.println();
-//                    System.out.print("Enter column again: ");
-//                    scanner.next(); // this is important!
-//                }
-//                pos2 = scanner.nextInt() -1;
-//            } while (pos2 < -1);
-//
-//            System.out.println("Enter positive number coordinates of card B (1-" + 4 + "), (1-" + 4 + "): ");
-//
-//            do {
-//                System.out.print("Please enter a row: ");
-//                while (!scanner.hasNextInt()) {
-//                    System.out.println("That's not a number!");
-//                    System.out.println();
-//                    System.out.print("Enter row again: ");
-//                    scanner.nextLine(); // this is important!
-//                }
-//                pos3 = scanner.nextInt() - 1;
-//            } while (pos3 < - 1);
-//
-//            do {
-//                System.out.print("Please enter a column: ");
-//                while (!scanner.hasNextInt()) {
-//                    System.out.println("That's not a number!");
-//                    System.out.println();
-//                    System.out.print("Enter column again: ");
-//                    scanner.nextLine(); // this is important!
-//                }
-//                pos4 = scanner.nextInt() - 1;
-//            } while (pos4 < - 1);
-//
-//            play.guess(pos1, pos2, pos3, pos4);
-//        }
-
         //If user wins show whole field and ask for playing Again.
         System.out.println("You have won!");
         System.out.println();
@@ -218,7 +167,6 @@ public class Main {
             play.setCounter(0);
             play.resetGame();
             flag = true;
-
         }
     }
         System.out.println("Thanks for playing!");
