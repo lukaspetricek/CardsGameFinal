e036063d6650e9ad0bcf20807990049706c744dc
Lukas Petricek <lukas.petricek.cz@gmail.com>
2022-10-20 09:02:32 +0200

    handled input, need more testing, clearing duplicated

--- src/GuessinTools.java
+++ src/GuessinTools.java
@@ -105,7 +105,7 @@ public class GuessinTools {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 showUser[i][j] = '#';
-                System.out.print(showUser[i][j]);
+                System.out.print(showUser[i][j] + " ");
             }
             System.out.println("");
         }
