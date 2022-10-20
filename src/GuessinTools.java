84c90be3894f587bd4a41646864cb8f1be7b9db0
Lukas Petricek <lukas.petricek.cz@gmail.com>
2022-10-20 10:50:35 +0200

    handled input, cleared warning messages

--- src/GuessinTools.java
+++ src/GuessinTools.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
-
 public class GuessinTools {
 
     public GuessinTools() {
@@ -49,39 +48,33 @@ public class GuessinTools {
     }
 
     public void guess(int pos1, int pos2, int pos3, int pos4) throws InterruptedException {
-        if ((pos1 < 4) && (pos2 < 4) && (pos3 < 4) && (pos4 < 4)) {
-            if (guessingField[pos1][pos2] != '#' && guessingField[pos3][pos4] != '#') {
-                if ((pos1 == pos3 && pos2 == pos4)) {
-                    System.out.println("Enter different card coordinates!");
-                    System.out.println();
-                    showPlayingField();
-                } else if (guessingField[pos1][pos2] == guessingField[pos3][pos4]) {
-                    showUser[pos1][pos2] = guessingField[pos1][pos2];
-                    showUser[pos3][pos4] = guessingField[pos3][pos4];
-                    guessingField[pos3][pos4] = '#';
-                    guessingField[pos1][pos2] = '#';
-
-                    System.out.println("Correct!");
-                    counter++;
-
-                    showPlayingField();
-                } else {
-                    System.out.println("Inccorect!");
-                    System.out.println();
-                    showUser[pos1][pos2] = guessingField[pos1][pos2];
-                    showUser[pos3][pos4] = guessingField[pos3][pos4];
-                    showPlayingField();
-                    TimeUnit.SECONDS.sleep(5);
-                    System.out.println();
-                    showUser[pos1][pos2] = '#';
-                    showUser[pos3][pos4] = '#';
-                    showPlayingField();
-                }
+        if (guessingField[pos1][pos2] != '#' && guessingField[pos3][pos4] != '#') {
+            if ((pos1 == pos3 && pos2 == pos4)) {
+                System.out.println("Enter different card coordinates!");
+                System.out.println();
+                showPlayingField();
+            } else if (guessingField[pos1][pos2] == guessingField[pos3][pos4]) {
+                showUser[pos1][pos2] = guessingField[pos1][pos2];
+                showUser[pos3][pos4] = guessingField[pos3][pos4];
+                guessingField[pos3][pos4] = '#';
+                guessingField[pos1][pos2] = '#';
+                System.out.println("Correct!");
+                counter++;
+                  showPlayingField();
             } else {
-                System.out.println("You have already guessed this combination!");
+                System.out.println("Inccorect!");
+                System.out.println();
+                showUser[pos1][pos2] = guessingField[pos1][pos2];
+                showUser[pos3][pos4] = guessingField[pos3][pos4];
+                showPlayingField();
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println();
+                showUser[pos1][pos2] = '#';
+                showUser[pos3][pos4] = '#';
+                showPlayingField();
             }
         } else {
-            System.out.println("Enter valid numbers!");
+            System.out.println("You have already guessed this combination!");
         }
     }
 
@@ -106,7 +99,7 @@ public class GuessinTools {
                 showUser[i][j] = '#';
                 System.out.print(showUser[i][j] + " ");
             }
-            System.out.println("\n");
+            System.out.println("");
         }
     }
 }
